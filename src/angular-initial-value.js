1deadef4c47dd2b43d9b855a699d8866c2405da8
Glauco Custódio <glauco.custodio@gmail.com>
2015-11-13 17:04:45 -0200

    Adding support for select multiple and input type date

--- src/angular-initial-value.js
+++ src/angular-initial-value.js
@@ -1,22 +1,22 @@
 var initialValueModule = angular.module('initialValue', [])
 .directive('initialValue', function() {
   var removeIndent = function (str) {
-		var arr = str.split("\n");
 		var result = "";
-		arr.forEach(function (it) {
-			result += it.trim();
-			result += '\n';
-		});
-
+    if(str && typeof(str) === 'string') {
+      var arr = str.split("\n");
+      arr.forEach(function (it) {
+        result += it.trim();
+        result += '\n';
+      });
+    }
 		return result;
 	};
-
 
   return{
     restrict: 'A',
     controller: ['$scope', '$element', '$attrs', '$parse', function($scope, $element, $attrs, $parse){
 
-      var getter, setter, val, tag;
+      var getter, setter, val, tag, values;
       tag = $element[0].tagName.toLowerCase();
       val = $attrs.initialValue || removeIndent($element.val());
       
@@ -29,11 +29,16 @@ var initialValueModule = angular.module('initialValue', [])
           val = ($element.val() !== undefined) ? parseFloat($element.val()) : undefined;
         } else if($element.attr('type') === 'color' || $element.attr('type') === 'range'){
           val = $element[0].getAttribute('value');
+        } else if($element.attr('type') === 'date') {
+          val = new Date(val.trim());
         }
       } else if(tag === "select"){
+        values = [];
         for (i=0; i < $element[0].options.length; i++) {
           option = $element[0].options[i];
-          if(option.hasAttribute('selected')) {
+          if(option.hasAttribute('selected') && $element[0].hasAttribute('multiple')) {
+            values.push(option.text);
+          } else {
             val = option.text;
           }
         }
@@ -42,7 +47,7 @@ var initialValueModule = angular.module('initialValue', [])
       if($attrs.ngModel){
         getter = $parse($attrs.ngModel);
         setter = getter.assign;
-        setter($scope, val);
+        setter($scope, values != undefined && values.length ? values : val);
       }
     }]
   };
